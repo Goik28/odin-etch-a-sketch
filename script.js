08b79713c45e0ab0ae1f5751303d1f3c31ad8749
Samuel <sbabinski@outlook.com>
2021-12-18 17:32:30 -0300

    Created all those files and reutilized the base layout and ungly css of the rock-paper-scissors game

---
+++ script.js
@@ -0,0 +1,5 @@
+//function to create all the divs based on the button. small for default
+//function to add event listener for all those divs
+//function to change the background color for the color picked
+//function to change the background color randomly based on the button pushed.
+//function to clear, default white.
